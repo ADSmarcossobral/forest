99bc052c3a86832e41da9c89e16c7ecc9cf59ece
Marcos Sobral <marcossobral94@gmail.com>
2017-09-29 21:54:23 -0300

    adding delete_node in red_black_tree

--- forest/red_black_tree.h
+++ forest/red_black_tree.h
@@ -423,6 +423,91 @@ namespace forest {
                                 return false;
                         }
                 }
+                /**
+                 * @brief Deletes a node with a specific key
+                 * @param key The key for node deletetion
+                 * @return void
+                 */
+                void delete_node(key_t key) {
+                        red_black_tree_node <key_t, value_t> *x = root;
+                        while(x != nullptr){
+                                if(key < x->key){
+                                        x = x->left;
+                                }else if(key > x->key){
+                                        x = x->right;
+                                }else{
+                                        if(x->left == nullptr && x->right == nullptr){
+                                                if(x->parent == nullptr)
+                                                        root = nullptr;
+                                                else if(x->parent->right != nullptr){
+                                                        if(x->parent->right->key == x->key)
+                                                                x->parent->right = nullptr;
+                                                        else
+                                                                x->parent->left = nullptr;
+                                                }else
+                                                        x->parent->left = nullptr;
+                                                delete(x);
+                                        }else if(x->left == nullptr){
+                                                if(x->parent != nullptr){
+                                                        x->right->parent = x->parent;
+                                                        if(x->parent->right != nullptr){
+                                                                if(x->parent->right->key == x->key)
+                                                                        x->parent->right = x->right;
+                                                                else
+                                                                        x->parent->left = x->right;
+                                                        } else{
+                                                                x->parent->left = x->right;
+                                                        }
+                                                }else{
+                                                        x->right->parent = nullptr;
+                                                        root = x->right;
+                                                }
+                                                delete(x);
+                                        } else if(x->right == nullptr){
+                                                if(x->parent != nullptr){
+                                                        x->left->parent = x->parent;
+                                                        if(x->parent->right != nullptr){
+                                                                if(x->parent->right->key == x->key)
+                                                                        x->parent->right = x->left;
+                                                                else
+                                                                        x->parent->left = x->left;
+                                                        }else{
+                                                                x->parent->left = x->left;
+                                                        }
+                                                }else{
+                                                        x->left->parent = nullptr;
+                                                        root = x->left;
+                                                }
+                                                delete(x);
+                                        } else{
+                                                red_black_tree_node <key_t, value_t> *child = x->left;
+                                                while(child->right != nullptr)
+                                                        child = child->right;
+                                                x->value = child->value;
+                                                x->key = child->key;
+                                                if(child->left != nullptr){
+                                                        if(child->parent->left->key == child->key){
+                                                                child->parent->left = child->left;
+                                                                child->left->parent = child->parent;
+                                                        }else{
+                                                                child->parent->right = child->left;
+                                                                child->left->parent = child->parent;
+                                                        }
+                                                } else{
+                                                        if(child->parent->left != nullptr){
+                                                                if(child->parent->left->key == child->key)
+                                                                        child->parent->left = nullptr;
+                                                                else
+                                                                        child->parent->right = nullptr;
+                                                        }else{
+                                                                child->parent->right = nullptr;
+                                                        }
+                                                }
+                                                delete(child);
+                                        }
+                                }
+                        }
+                }
         };
 }
 
